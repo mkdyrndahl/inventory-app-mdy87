cf380b7927c104a9090c45dd137d62e27ab3760a
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-03-21 14:16:03 -0700

    "Created skeleton of the main application using create-react-app, added the base for the inventory service"

---
+++ src/Services/inventoryService.js
@@ -0,0 +1,26 @@
+
+export function getInventory()  {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}`)
+    .then(response => response.json())
+}
+
+export function addToCart(item) {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}`,   
+    {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({"sku" : item.sku, "name": item.name, "price": item.price, "quantity": item.quantity})
+    })
+    .then(response => response.json())
+}
+
+export async function deleteItem(sku){
+    const deleteData = JSON.stringify({"sku": sku})
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}`,
+    {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({"sku" : item.sku, "name": item.name, "price": item.price, "quantity": item.quantity})
+    })
+    .then(response => response.json())
+}
